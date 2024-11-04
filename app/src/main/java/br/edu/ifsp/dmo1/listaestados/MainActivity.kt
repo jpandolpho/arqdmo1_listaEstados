a284a62446ee213c2c0b6da4626e6edf92ff43aa
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-04 09:15:00 -0300

    Adicionando um listener aos itens da lista.

--- app/src/main/java/br/edu/ifsp/dmo1/listaestados/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/listaestados/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.edu.ifsp.dmo1.listaestados
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.dmo1.listaestados.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
     private lateinit var binding: ActivityMainBinding
     private val states = arrayOf("AC","AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PE", "PR", "PI", "RJ", "RN", "RS", "RR", "SC", "SP", "SE", "TO")
 
@@ -18,5 +22,15 @@ class MainActivity : AppCompatActivity() {
         val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, states)
         //conectando a view com o encapsulador
         binding.listview.adapter = adapter
+
+        binding.listview.onItemClickListener = this
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(
+            this,
+            "Você clicou em ${states.get(position)}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
